5df7cd5f8f05ac4fbadaafa53df981e04d35bed3
sundisaab <166897100+sundisaab@users.noreply.github.com>
2024-05-28 21:09:40 +0530

    Add files via upload

---
+++ face detect.py
@@ -0,0 +1,23 @@
+import cv2
+
+face_cap =cv2.CascadeClassifier("sample.xml")
+video_cap = cv2.VideoCapture(0)
+while True:
+   
+   ret, video_data = video_cap.read()
+   col = cv2.cvtColor(video_data,cv2.COLOR_BGR2GRAY)
+   faces = face_cap.detectMultiScale(
+       col,
+       scaleFactor=1.1,
+       minNeighbors=5,
+       minSize=(30,30),
+       flags=cv2.CASCADE_SCALE_IMAGE
+
+   )
+
+   
+   cv2.imshow("video_live", video_data)
+   if cv2.waitKey(10) == ord("a"):
+       break
+
+video_cap.release()
